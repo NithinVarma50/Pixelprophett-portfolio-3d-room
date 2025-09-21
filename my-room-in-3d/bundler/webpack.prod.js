29b8215276643aa6cb1da3018e1b342374ea72c8
Pixelprophett <nv787265@gmail.com>
2025-09-21 21:36:06 +0530

    Add Vercel config, Babel, and enhance prod build
    Introduces .vercelignore and updates vercel.json for improved deployment and asset caching. Adds .babelrc for browser compatibility. Enhances webpack.prod.js with CSS extraction, asset copying, chunk splitting, and performance hints. Updates package.json with new scripts, dependencies, and project metadata.

--- my-room-in-3d/bundler/webpack.prod.js
+++ my-room-in-3d/bundler/webpack.prod.js
@@ -1,14 +1,64 @@
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
 
 module.exports = merge(
     commonConfiguration,
     {
         mode: 'production',
+        devtool: false,
+        output: {
+            path: path.resolve(__dirname, '../dist'),
+            filename: 'bundle.[contenthash].js',
+            publicPath: '/',
+            clean: true
+        },
         plugins:
         [
-            new CleanWebpackPlugin()
-        ]
+            new CleanWebpackPlugin(),
+            new MiniCSSExtractPlugin({
+                filename: '[name].[contenthash].css'
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, '../static'),
+                        to: path.resolve(__dirname, '../dist/assets'),
+                        noErrorOnMissing: true
+                    }
+                ]
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(css)$/,
+                    use: [
+                        MiniCSSExtractPlugin.loader,
+                        'css-loader'
+                    ]
+                }
+            ]
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all',
+                    }
+                }
+            }
+        },
+        performance: {
+            hints: 'warning',
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+        }
     }
 )
