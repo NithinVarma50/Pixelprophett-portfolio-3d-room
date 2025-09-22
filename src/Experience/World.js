be1b323e9578681f075f38b68b7a917128f1a168
Pixelprophett <nv787265@gmail.com>
2025-09-22 16:47:04 +0530

    Update World.js

--- src/Experience/World.js
+++ src/Experience/World.js
@@ -76,12 +76,12 @@ export default class World
         this.pcScreen = new Screen(
             this.resources.items.pcScreenModel.scene.children[0],
             '/assets/unnamed.png',
-            { cacheBust: true, flipVertical: true }
+            { cacheBust: true }
         )
         this.macScreen = new Screen(
             this.resources.items.macScreenModel.scene.children[0],
             '/assets/unnamed.png',
-            { cropAspect: 16 / 10, cacheBust: true, flipVertical: true }
+            { cropAspect: 16 / 10, cacheBust: true }
         )
     }
 
